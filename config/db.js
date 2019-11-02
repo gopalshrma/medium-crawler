73483c1fe8e5cedc28e5c4d64acc85a9ad9ef62e
Gopal Sharma <gopal131072@gmail.com>
2019-11-03 03:13:11 +0530

    Added DB_CONNECTION to env variables

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 let mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
-const DB_CONNECTION = process.env.DB_CONNECTION || 'mongodb://localhost:27017/rentomojo';
+const DB_CONNECTION = require('./config').DB_CONNECTION || 'mongodb://localhost:27017/rentomojo';
 
 mongoose = mongoose.createConnection(DB_CONNECTION,
     {
